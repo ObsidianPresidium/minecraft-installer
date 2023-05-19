664d58ce732712bc15dafe193e83c7baab13d4d5
ObsidianPresidium <emil@aarestrup.nu>
2023-05-19 17:00:18 +0200

    edit: make build target dynamic, update for base64

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 PREVIOUS_WORKING_DIRECTORY=$PWD
+BUILD_TARGET="minecraft-installer-build.sh"
 cd "$(dirname $0)"
 tar -czvf ./archive.tar.gz ./archive/
-cat ./minecraft-installer.sh >> ./minecraft-installer-build.sh
-cat ./archive.tar.gz >> ./minecraft-installer-build.sh
-chmod +x ./minecraft-installer-build.sh
+cat ./minecraft-installer.sh >> ./$BUILD_TARGET
+base64 ./archive.tar.gz >> ./$BUILD_TARGET
+rm ./archive.tar.gz
+chmod +x ./$BUILD_TARGET
 cd $PREVIOUS_WORKING_DIRECTORY
+echo "Build complete! Saved to $BUILD_TARGET"
 exit 0
