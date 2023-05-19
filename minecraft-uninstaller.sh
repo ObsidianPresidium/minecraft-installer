2a710191d141340e8b9009562218abd61dc5c257
ObsidianPresidium <emil@aarestrup.nu>
2023-05-19 17:02:10 +0200

    add: user-friendly output

--- minecraft-uninstaller.sh
+++ minecraft-uninstaller.sh
@@ -3,3 +3,4 @@ sudo rm -r /usr/share/minecraft
 sudo rm /usr/share/applications/Minecraft.desktop
 sudo rm /usr/share/pixmaps/minecraft.svg
 sudo rm -f /bin/minecraft-launcher
+echo "Minecraft is now uninstalled."
