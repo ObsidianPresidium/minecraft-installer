01b6aa279a64de035ffb2d51e656c09994f6f8cc
ObsidianPresidium <emil@aarestrup.nu>
2023-05-19 17:01:45 +0200

    fix: archive extraction not working, minecraft-launcher not being set as executable

--- minecraft-installer.sh
+++ minecraft-installer.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-ARCHIVE_LINE=$(awk '/^__ARCHIVE_BEGINS__/ { print NR + 1; exit 0; }' $0)
+ARCHIVE_LINE=$(awk '/^__ARCHIVE__/ { print NR + 1; exit 0; }' $0)
+
 PREVIOUS_WORKING_DIRECTORY=$PWD
 
 # Get Minecraft
 sudo mkdir -p /tmp/minecraft-installer
+# Extract included archive. This must be done before any cd commands.
+sudo bash -c "tail -n +${ARCHIVE_LINE} $0 | base64 -d | tar -zpx -C /tmp/minecraft-installer/"
 cd /tmp/minecraft-installer
 sudo wget -qO ./minecraft.tar.gz "https://launcher.mojang.com/download/Minecraft.tar.gz"
-# Extract Minecraft and included archive
+# Extract Minecraft
 sudo tar -xzf ./minecraft.tar.gz
-tail -n +${ARCHIVE_LINE} $0 | tar -zpvx -C /tmp/minecraft-installer/
 # Install into proper folders
 sudo mkdir -p /usr/share/minecraft
-sudo install -Dm 644 ./minecraft-launcher/minecraft-launcher /usr/share/minecraft/minecraft-launcher
+sudo install -Dm 755 ./minecraft-launcher/minecraft-launcher /usr/share/minecraft/minecraft-launcher
 sudo install -Dm 644 ./archive/Minecraft.desktop /usr/share/applications/Minecraft.desktop
 sudo install -Dm 644 ./archive/minecraft.svg /usr/share/pixmaps/minecraft.svg
 # Symlink minecraft-launcher into /bin
@@ -20,6 +22,9 @@ sudo ln -sf /usr/share/minecraft/minecraft-launcher /bin/minecraft-launcher
 # Clean up
 cd "$PREVIOUS_WORKING_DIRECTORY"
 sudo rm -r /tmp/minecraft-installer
+
+echo "Done! Minecraft should now be installed. You can open it with your usual menu system, or by executing minecraft-launcher in a terminal."
 exit 0
 
-__ARCHIVE_BEGINS__
+# This next line must have a newline after it.
+__ARCHIVE__
